7f4540c373cb0715e752ab80a19f1a60e01597e0
Victor Elezua <victorelezua@gmail.com>
2020-07-24 16:15:37 +0100

    add loading animation

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,40 @@ const BASE_API_URL = "https://api.github.com/search/users";
 
 const searchTextInput = document.getElementById("search-text");
 const searchBtn = document.getElementById("search-btn");
+const spinner = document.getElementById("spinner");
+
+function toggleAnimation() {
+    let state = spinner.style.visibility;
+    
+    const toggleVisibility = () => {
+        if (state == "" || state == "hidden") {
+            state = "visible";
+            spinner.style.visibility = state;
+        } else {
+            state = "hidden";
+            spinner.style.visibility = state;
+        }
+    };
+    
+    return {
+        toggleVisibility
+    };
+}
+
+const anim = toggleAnimation();
+
+function toggleInputsDisabled() {
+    searchTextInput.disabled = !searchTextInput.disabled;
+    searchBtn.disabled = !searchBtn.disabled;
+    anim.toggleVisibility();
+}
 
 searchBtn.addEventListener('click', (event) => {
+    toggleInputsDisabled();
     getUsersByLocation(searchTextInput.value).then(function(res) {
         res.json().then(data => {
             console.log(data);
+            toggleInputsDisabled();
         });
     });
     searchTextInput.value = "";
