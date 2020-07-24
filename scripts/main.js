487d170866ef457f0f846b79767f7aadfc61be0b
Victor Elezua <victorelezua@gmail.com>
2020-07-24 15:12:28 +0100

    add basic http functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,18 @@
-console.log("Welcome to tech users");
+const BASE_API_URL = "https://api.github.com/search/users";
+
+const searchTextInput = document.getElementById("search-text");
+const searchBtn = document.getElementById("search-btn");
+
+searchBtn.addEventListener('click', (event) => {
+    getUsersByLocation(searchTextInput.value).then(function(res) {
+        res.json().then(data => {
+            console.log(data);
+        });
+    });
+    searchTextInput.value = "";
+});
+
+async function getUsersByLocation(loc) {
+    const result = await fetch(`${BASE_API_URL}?q=location:${loc}`);
+    return result;
+}
